1039f136bfbaf2617ef3ed3890d197e94304e6d4
Kendal Lopez <klopez80241@ufide.ac.cr>
2026-01-28 14:38:31 -0600

    Agrego proyecto Tienda_kendal

---
+++ Tienda_kendal/src/main/java/Tienda_kendal/demo/TiendakendalApplication.java
@@ -0,0 +1,13 @@
+package Tienda_kendal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendakendalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendakendalApplication.class, args);
+	}
+
+}
